0a2f029a5aab4d142fd03266e1cc8fa30d47e6f3
LalitTurbot <lalit@turbot.com>
2021-05-20 16:31:35 +0530

    update section 6 queries for subscription name

--- query/azure_sql/sql_database_allow_internet_access.sql
+++ query/azure_sql/sql_database_allow_internet_access.sql
@@ -1,6 +1,6 @@
 select
   -- Required Columns
-  id resource,
+  sqls.id resource,
   case
     when firewall_rules @> '[{"properties":{"endIpAddress":"0.0.0.0","startIpAddress":"0.0.0.0"}}]'
     or firewall_rules @> '[{"properties":{"endIpAddress":"0.0.0.0","startIpAddress":"255.255.255.255"}}]'
@@ -10,11 +10,12 @@ select
   case
     when firewall_rules @> '[{"properties":{"endIpAddress":"0.0.0.0","startIpAddress":"0.0.0.0"}}]'
     or firewall_rules @> '[{"properties":{"endIpAddress":"0.0.0.0","startIpAddress":"255.255.255.255"}}]'
-      then title || ' allows ingress 0.0.0.0/0 or any ip over internet.'
-      else title || ' not allows ingress 0.0.0.0/0 or any ip over internet.'
+      then sqls.title || ' allows ingress 0.0.0.0/0 or any ip over internet.'
+      else sqls.title || ' not allows ingress 0.0.0.0/0 or any ip over internet.'
   end as reason,
   -- Additional Dimensions
   resource_group,
-  split_part(subscription_id, '-', 5) as subscription_id
+  coalesce(sub.display_name, split_part(sqls.subscription_id, '-', 5)) as subscription
 from
-  azure_sql_server;
+  azure_sql_server sqls
+  join azure_subscription sub on sub.subscription_id = sqls.subscription_id;

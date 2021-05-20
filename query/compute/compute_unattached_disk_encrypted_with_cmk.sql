f5979ba81e222d48e0c1f616887ad3265d737709
LalitTurbot <lalit@turbot.com>
2021-05-20 16:09:29 +0530

    update compute queries for subscription name

--- query/compute/compute_unattached_disk_encrypted_with_cmk.sql
+++ query/compute/compute_unattached_disk_encrypted_with_cmk.sql
@@ -1,18 +1,19 @@
 select
   -- Required Columns
-  id as resource,
+  disk.id as resource,
   case
     when encryption_type = 'EncryptionAtRestWithCustomerKey' then 'ok'
     else 'alarm'
   end as status,
   case
-    when encryption_type = 'EncryptionAtRestWithCustomerKey' then name || ' encrypted with CMK.'
-    else name || ' not encrypted with CMK.'
+    when encryption_type = 'EncryptionAtRestWithCustomerKey' then disk.name || ' encrypted with CMK.'
+    else disk.name || ' not encrypted with CMK.'
   end as reason,
   -- Additional Dimensions
   resource_group,
-  split_part(subscription_id, '-', 5) as subscription_id
+  coalesce(sub.display_name, split_part(disk.subscription_id, '-', 5)) as subscription
 from
-  azure_compute_disk
+  azure_compute_disk disk
+  join azure_subscription sub on sub.subscription_id = disk.subscription_id
 where
   disk_state != 'Attached';
--- query/compute/compute_vm_utilizing_managed_disk.sql
+++ query/compute/compute_vm_utilizing_managed_disk.sql
@@ -1,16 +1,17 @@
 select
   -- Required Columns
-  id as resource,
+  vm.id as resource,
   case
     when managed_disk_id is null then 'alarm'
     else 'ok'
   end as status,
   case
-    when managed_disk_id is null then name || ' VM not utilizing managed disks.'
-    else name || ' VM utilizing managed disks.'
+    when managed_disk_id is null then vm.name || ' VM not utilizing managed disks.'
+    else vm.name || ' VM utilizing managed disks.'
   end as reason,
   -- Additional Dimensions
   resource_group,
-  split_part(subscription_id, '-', 5) as subscription_id
+  coalesce(sub.display_name, split_part(vm.subscription_id, '-', 5)) as subscription
 from
-  azure_compute_virtual_machine;
+  azure_compute_virtual_machine vm
+  join azure_subscription sub on sub.subscription_id = vm.subscription_id

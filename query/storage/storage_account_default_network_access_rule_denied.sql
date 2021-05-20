6ced3045a3e8a423968484e9881ec7cebf9b9cd9
LalitTurbot <lalit@turbot.com>
2021-05-20 15:38:42 +0530

    update

--- query/storage/storage_account_default_network_access_rule_denied.sql
+++ query/storage/storage_account_default_network_access_rule_denied.sql
@@ -11,7 +11,7 @@ select
   end as reason,
   -- Additional Dimensions
   resource_group,
-  coalesce(sub.display_name, split_part(sa.subscription_id, '-', 5) as subscription_id) as subscription
+  coalesce(sub.display_name, split_part(sa.subscription_id, '-', 5)) as subscription
 from
   azure_storage_account sa
   join azure_subscription sub on sub.subscription_id = sa.subscription_id;

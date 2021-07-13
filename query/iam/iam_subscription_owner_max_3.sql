1fb9773f1c76518791cab7b57696670add387d0f
Krishna <krishna@Admins-MacBook-Pro.local>
2021-07-13 14:06:11 +0530

    Update query files

---
+++ query/iam/iam_subscription_owner_max_3.sql
@@ -0,0 +1,32 @@
+with owner_roles as (
+  select
+    role_name,
+    role_type, name,
+    title,
+    subscription_id
+  from
+    azure_role_definition
+  where
+    role_name = 'Owner'
+)
+select
+  -- Required columns
+  owner.subscription_id as resource,
+  case
+    when count(*) >= 3 then 'ok'
+    else 'alarm'
+  end as status,
+  case
+    when count(*) = 1 then 'There is 1 owner.'
+    when count(*) > 1 then 'There are ' || count(*) || ' owners.'
+  end as reason,
+  -- Additional Dimensions
+  sub.display_name as subscription
+from
+  owner_roles owner,
+  azure_subscription sub
+where
+  sub.subscription_id =owner.subscription_id
+group by
+  owner.subscription_id,
+  sub.display_name;

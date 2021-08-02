b345d10d5dffdda130cc70a01cbd3d6c5d0c8d36
Khushboo <khushboo@turbot.com>
2021-08-02 18:30:01 +0530

    Added queries for app service cors

---
+++ query/appservice/appservice_web_app_cors_no_star.sql
@@ -0,0 +1,20 @@
+select
+  -- Required Columns
+  a.id as resource,
+  case
+    when configuration -> 'properties' -> 'cors' -> 'allowedOrigins' @> '["*"]' then 'alarm'
+    else 'ok'
+  end as status,
+  case
+    when configuration -> 'properties' -> 'cors' -> 'allowedOrigins' @> '["*"]'
+      then a.name || ' CORS allow all domains to access the application.'
+    else a.name || ' CORS does not all domains to access the application.'
+  end as reason,
+  -- Additional Dimensions
+  resource_group,
+  sub.display_name as subscription
+from
+  azure_app_service_web_app a,
+  azure_subscription sub
+where
+  sub.subscription_id = a.subscription_id;

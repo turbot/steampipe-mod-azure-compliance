3ff4f8e935b1d67de55f5e125662b5b9dd777e44
misraved <ved@turbot.com>
2022-01-05 16:19:41 +0530

    Update query to include additional gateway and subnet checks

--- query/network/network_ddos_enabled.sql
+++ query/network/network_ddos_enabled.sql
@@ -1,19 +1,27 @@
+with application_gateway_subnet as (
+  select
+		distinct ( split_part(c -> 'properties' -> 'subnet' ->> 'id', '/', 9) ) as vn_name
+	from
+		azure_application_gateway as ag,
+		jsonb_array_elements(gateway_ip_configurations) as c
+)
 select
   -- Required Columns
-  avn.name as resource,
+  a.name as resource,
   case
-    when enable_ddos_protection::bool then 'ok'
+    when b.vn_name is null then 'ok'
+    when b.vn_name is not null and enable_ddos_protection::bool then 'ok'
     else 'alarm'
   end as status,
   case
-    when enable_ddos_protection::bool then avn.name || ' DDoS protection enabled.'
-    else avn.name || ' DDoS protection disabled.'
+    when b.vn_name is null then 'DDoS protection not required.'
+    when b.vn_name is not null and enable_ddos_protection::bool then a.name || ' DDoS protection enabled.'
+    else a.name || ' DDoS protection disabled.'
   end as reason,
   -- Additional Dimensions
   resource_group,
   sub.display_name as subscription
 from
-  azure_virtual_network avn,
-  azure_subscription sub
-where
-  sub.subscription_id = avn.subscription_id; 
+  azure_virtual_network as a
+  left join application_gateway_subnet as b on a.name = b.vn_name
+  join azure_subscription sub on sub.subscription_id = a.subscription_id;

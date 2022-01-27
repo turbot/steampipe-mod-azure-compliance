c2f43ea9c92c1a33c13a8172efec9c10d400fe52
misraved <ved@turbot.com>
2022-01-27 15:06:45 +0530

    Fix the hdinsight_cluster_encrypted_at_rest_with_cmk query

--- query/hdinsight/hdinsight_cluster_encrypted_at_rest_with_cmk.sql
+++ query/hdinsight/hdinsight_cluster_encrypted_at_rest_with_cmk.sql
@@ -3,12 +3,12 @@ select
   a.id as resource,
   case
     when provisioning_state <> 'Succeeded' then 'skip'
-    when disk_encryption_properties -> 'keyName' is not null and provisioning_state <> 'Failed' then 'ok'
+    when disk_encryption_properties -> 'keyName' is not null then 'ok'
     else 'alarm'
   end as status,
   case
     when provisioning_state <> 'Succeeded' then a.name || ' is in ' || provisioning_state || ' state.'
-    when disk_encryption_properties -> 'keyName' is not null and provisioning_state <> 'Failed' then a.name || ' encrypted with CMK.'
+    when disk_encryption_properties -> 'keyName' is not null then a.name || ' encrypted with CMK.'
     else a.name || ' not encrypted with CMK.'
   end as reason,
   -- Additional Dimensions

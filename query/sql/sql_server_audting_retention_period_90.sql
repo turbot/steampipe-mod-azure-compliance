bc6d3e0bd8eb888208aeb68f218e24e6cec77cfc
rajmohanty17 <raj@turbot.com>
2022-05-11 19:19:16 +0530

    Fix false positive in query sql_server_audting_retention_period_90 if audit log retention day equals 0 Closes #104

--- query/sql/sql_server_audting_retention_period_90.sql
+++ query/sql/sql_server_audting_retention_period_90.sql
@@ -2,10 +2,12 @@ select
   -- Required Columns
   s.id as resource,
   case
+    when (audit -> 'properties' ->> 'retentionDays')::integer = 0 then 'ok'
     when (audit -> 'properties' ->> 'retentionDays')::integer <= 90 then 'alarm'
     else 'ok'
   end as status,
   case
+    when (audit -> 'properties' ->> 'retentionDays')::integer = 0 then name || ' audit retention set to unlimited days.'
     when (audit -> 'properties' ->> 'retentionDays')::integer <= 90 then name || ' audit retention less than 90 days.'
     else  name || ' audit retention greater than 90 days.'
   end as reason,

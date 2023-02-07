fb18db0c6737867b8786eef5c1c5656728dbd59d
madhushreeray30 <madhushree@turbot.com>
2023-02-07 15:38:47 +0530

    Update alarm to info in query kubernetes_cluster_os_and_data_disks_encrypted_with_cmk

--- query/kubernetes/kubernetes_cluster_os_and_data_disks_encrypted_with_cmk.sql
+++ query/kubernetes/kubernetes_cluster_os_and_data_disks_encrypted_with_cmk.sql
@@ -3,11 +3,11 @@ select
   c.id as resource,
   case
     when disk_encryption_set_id is not null then 'ok'
-    else 'alarm'
+    else 'info'
   end as status,
   case
-    when disk_encryption_set_id is not null then c.name || ' os and data diska encrypted with CMK.'
-    else c.name || ' os and data diska not encrypted with CMK.'
+    when disk_encryption_set_id is not null then c.name || ' os and data disks encrypted with CMK.'
+    else c.name || ' os and data disks not encrypted with CMK or client IP not whitelisted.'
   end as reason,
   -- Additional Dimensions
   resource_group,
